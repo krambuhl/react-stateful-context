3366b01b6b834df39f6c4f4a59ba7b91402d6ce7
Evan Krambuhl <thekrambuhl@gmail.com>
2018-08-10 19:07:55 -0700

    maybe a deep consumer at some point

---
+++ example/pages/deepContext.js
@@ -0,0 +1,50 @@
+import StatefulContext from '../../dist'
+
+export default () =>
+  <StatefulContext.Provider
+    getInitialState={{ count: 10 }}
+  >
+    <StatefulContext.Consumer>
+      {
+        (context) => {
+          const { count, setContextState } = context
+          return (
+            <div>
+              <div>Root Count: {count}</div>
+
+              <button onClick={() => setContextState({ count: count + 1 })}>Add 1</button>
+              <button onClick={() => setContextState({ count: count - 1 })}>Subtract 1</button>
+
+              <StatefulContext.Provider getInitialState={context}>
+                <StatefulContext.Consumer>
+                  {
+                    ({ count, setContextState }) => (
+                      <div>
+                        <div>Deep Count: {count}</div>
+
+                        <button onClick={() => setContextState({ count: count + 1 })}>Add 1</button>
+                        <button onClick={() => setContextState({ count: count - 1 })}>Subtract 1</button>
+                      </div>
+                    )
+                  }
+                </StatefulContext.Consumer>
+              </StatefulContext.Provider>
+
+              <StatefulContext.Consumer>
+                {
+                  ({ count, setContextState }) => (
+                    <div>
+                      <div>Root Count: {count}</div>
+
+                      <button onClick={() => setContextState({ count: count + 1 })}>Add 1</button>
+                      <button onClick={() => setContextState({ count: count - 1 })}>Subtract 1</button>
+                    </div>
+                  )
+                }
+              </StatefulContext.Consumer>
+            </div>
+          )
+        }
+      }
+    </StatefulContext.Consumer>
+  </StatefulContext.Provider>
