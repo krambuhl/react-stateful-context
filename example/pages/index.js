226dca3f7c45865d3bf19f66797b14120a0433af
Evan Krambuhl <thekrambuhl@gmail.com>
2018-08-10 17:48:12 -0700

    update example to next

---
+++ example/pages/index.js
@@ -0,0 +1,27 @@
+import StatefulContext from '../../dist/index.es'
+
+export default () =>
+  <div>
+    <StatefulContext.Provider
+      getInitialState={{ count: 0 }}
+    >
+      {/* Retrieve context state property */}
+      <StatefulContext.Consumer>
+        {
+          ({ count }) =>
+            <div>Current Count: {count}</div>
+        }
+      </StatefulContext.Consumer>
+
+      {/* Update context state property */}
+      <StatefulContext.Consumer>
+        {
+          ({ count, setContextState }) =>
+            <div>
+              <button onClick={() => setContextState({ count: count + 1 })}>Add 1</button>
+              <button onClick={() => setContextState({ count: count - 1 })}>Subtract 1</button>
+            </div>
+        }
+      </StatefulContext.Consumer>
+    </StatefulContext.Provider>
+  </div>
